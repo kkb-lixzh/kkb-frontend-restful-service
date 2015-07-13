14d2c8be3955ad9a6c08e26451cb424d92b912b8
lixzh <xzli@kaikeba.com>
2015-07-13 18:16:02 +0800

    修改命名空间

--- src/root.js
+++ src/root.js
@@ -1,21 +1,10 @@
 (function($w) {
-    var kkb = kkb || {};
+    var kkb = $w.kkb || {};
     kkb.restful = kkb.restful || {};
     kkb.restful.courses = {};
     kkb.restful.classes = {};
     kkb.restful.categories = {};
-    kkb.models = {};
-    kkb.models.courseModels = {};
+    kkb.models =kkb.models || {};
+    kkb.models.courseModels =kkb.models.courseModels || {};
     $w.kkb = kkb;
 })(window);
-
-var appModule = angular.module('kkb-restful-app', ['ngResource', 'ngCookies']);
-
-appModule.service('restful-course-comment', ['$resource', '$cookies', function($resource, $cookies) {
-    return new kkb.restful.courses.CommentService($resource, $cookies);
-}]);
-
-
-appModule.controller('CommentServiceCtrl', ['$scope', 'restful-course-comment', function($scope, commentService) {
-    $scope.text = 'Hello World!';
-}]);
